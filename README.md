0d514348afd34c3a8badefc6f36c7b878b98383e
Elvis Agbesi <elvisagbesi@gmail.com>
2020-12-16 23:02:56 +0000

    docs: :memo: Docs update

--- README.md
+++ README.md
@@ -8,7 +8,13 @@ npm start -  starts the application
 
 this is basically runnning in the browser and uses the underlying hardware to convert video files using FFmpeg.
 
-Example images below
+Example images below (converted the pictures using this, actually pretty dope)
+
+![gifer](./gifer.gif)
+
+and it also shows the logs if you set them when initializing FFmpeg. That makes you get to see what is happening at in DevTools
 
 
+![logger](./log.gif)
 
+Definately more experiments on Web Assembly!
--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
-const ffmpeg = createFFmpeg({log:true})
+const ffmpeg = createFFmpeg({ log: true })
 
 
 function App() {
@@ -12,53 +12,53 @@ function App() {
   const [video, setVideo] = useState();
   const [gif, setGif] = useState();
 
-  const load = async ( ) =>  {
+  const load = async () => {
     await ffmpeg.load()
     setReady(true)
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     load()
-  },[])
+  }, [])
 
   const convertToGif = async () => {
     // read the file that we stored
-    ffmpeg.FS('writeFile','catgif.mp4',await fetchFile(video))
+    ffmpeg.FS('writeFile', 'catgif.mp4', await fetchFile(video))
 
     // run a native CLI ffmpeg command
-    await ffmpeg.run('-i','catgif.mp4','-t','13.0','-ss','13.0','-f','gif','out.gif')
+    await ffmpeg.run('-i', 'catgif.mp4', '-t', '13.0', '-ss', '13.0', '-f', 'gif', 'out.gif')
 
     // read the data gotten
-    const data = ffmpeg.FS('readFile','out.gif')
+    const data = ffmpeg.FS('readFile', 'out.gif')
 
     // create a URL
-    const url = URL.createObjectURL(new Blob([data.buffer],{type: 'image/gif/'}))
+    const url = URL.createObjectURL(new Blob([data.buffer], { type: 'image/gif/' }))
     setGif(url)
   }
 
-  return ready ? ( 
+  return ready ? (
     <div className="App">
-      {video && 
+      {video &&
         <video controls
-        width="250"
-        src={URL.createObjectURL(video)} >
+          width="250"
+          src={URL.createObjectURL(video)} >
         </video>
       }
 
-      <input type="file" onChange={(e)=> setVideo(e.target.files?.item(0))}/>
+      <input type="file" onChange={(e) => setVideo(e.target.files?.item(0))} />
 
       <button onClick={convertToGif} >Convert</button>
 
-      {gif && 
-        <img 
-        width="250"
-        src={gif} />
+      {gif &&
+        <img
+          width="250"
+          src={gif} />
       }
     </div>
-  ): 
-  (
-    <p>Loading...</p>
-  )
+  ) :
+    (
+      <p>Loading...</p>
+    )
 }
 
 export default App;
